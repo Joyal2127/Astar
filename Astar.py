f400ca79e0c3a7b9ac324b3a1839b7ebb0cb062e
Joyal Puthur <150194916+Joyal2127@users.noreply.github.com>
2024-06-06 23:50:50 -0400

    Update Astar.py

--- Astar.py
+++ Astar.py
@@ -7,7 +7,7 @@ WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A* Path Finding Algorithm ")
 
 RED = (255, 0, 0)
-GREEN =(0, 0, 255)
+GREEN =(0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
@@ -26,7 +26,7 @@ class Spot:
         self.color = WHITE
         self.neighbors = []
         self.width = width
-        self.total = total_rows
+        self.total_rows = total_rows
 
     def get_pos(self):
         return self.row, self.col
