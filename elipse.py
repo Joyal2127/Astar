e6d9fde060a1d3acc3fd8eefe4c4211f0eade471
Joyal Puthur <150194916+Joyal2127@users.noreply.github.com>
2024-06-11 19:34:31 -0400

    Create elipse.py

---
+++ elipse.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+a = 13
+b = 10
+
+
+C = np.pi * (3 * (a + b) - np.sqrt((3 * a + b) * (a + 3 * b)))
+
+print(f"The circumference of the ellipse is approximately {C:.2f} cm.")
